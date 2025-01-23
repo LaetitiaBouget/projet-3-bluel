bf08c51b6f161b857c9957b4a79726b3410688b5
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-01-23 18:05:23 +0100

    mode-edit

---
+++ FrontEnd/js/edit.js
@@ -0,0 +1,55 @@
+const authToken = localStorage.getItem('authToken');
+const body = document.querySelector('body'); 
+const navLogin = document.getElementById("nav-login");
+const header = document.querySelector("header");
+const h2 = document.querySelector("#portfolio h2");
+const portfolioSectionEdit = document.querySelector("#portfolio");
+
+
+function createBannerEdit(){
+    const bannerEdit = document.createElement("div");
+    bannerEdit.classList.add("bandeau-edit")
+    header.insertAdjacentElement("beforebegin", bannerEdit);
+    const textBannerEdit = document.createElement("p");
+    textBannerEdit.innerHTML = `<i class="fa-solid fa-pen-to-square"></i> Mode Édition`
+    bannerEdit.appendChild(textBannerEdit);
+    return bannerEdit
+}
+
+function createWorksEdit(){
+    const worksEdit = document.createElement("p");
+    worksEdit.innerHTML = `<i class="fa-solid fa-pen-to-square"></i> modifier`
+    worksEdit.classList.add("travaux-edit")
+    h2.insertAdjacentElement("afterend", worksEdit);
+    return worksEdit
+}
+
+function modeEdit(bannerEdit, worksEdit){
+    if (authToken !== null) {
+        body.classList.add('mode-edition');
+        navLogin.innerText = "logout";
+        navLogin.setAttribute("href", "#");
+        bannerEdit.style.display = "block";
+        worksEdit.style.display = "inline-block";
+
+    }  else {
+
+        body.classList.remove('mode-edition');
+        navLogin.innerText = "login";
+        navLogin.setAttribute("href", "login.html");
+        bannerEdit.style.display = "none";
+        worksEdit.style.display = "none";
+    }
+}
+
+function logout(){
+    navLogin.addEventListener("click", function (event) {
+        if (authToken !== null) {
+            localStorage.clear(); 
+            window.location.href = "./index.html";
+        }
+    });
+}
+
+modeEdit(createBannerEdit (), createWorksEdit())
+logout();
