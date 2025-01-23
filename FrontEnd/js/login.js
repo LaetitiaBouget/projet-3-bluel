a1c13cc73e099a36d29b7e02c6299334e21c5655
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-01-23 12:44:15 +0100

    login

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,34 @@
+const formulaireLogin = document.querySelector("#login-form");
+formulaireLogin.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const user = {
+        email: event.target.querySelector("[name=email]").value,
+        password: event.target.querySelector("[name=password]").value,
+    };
+
+    const response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+
+const result = await response.json();
+
+if (response.ok) {
+    
+    localStorage.setItem('authToken', result.token);
+
+    window.location.href = "./index.html";
+}
+else{
+    alert("Erreur dans l’identifiant ou le mot de passe");
+}
+
+});
+
+
+
