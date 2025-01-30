60eb16ad5a6edc8b0994404dfdb1eab7122004f8
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-01-30 17:59:34 +0100

    ouverture-fermeture-modale

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,103 +1,118 @@
-
-//Ouverture et fermeture de la modale
-
 let modal = null
 
-const openModal = function(e) {
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
-    target.style.display = "block"
-    modal = target
-    modal.addEventListener ("click", closeModal)
-    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
-    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+function openModal (e){
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute("href"));
+    target.style.display = "block";
+    modal = target;
+
+    modal.addEventListener ("click", closeModal);
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+
+    const addWorkBtn = modal.querySelector(".add-work-btn");
+    addWorkBtn.addEventListener("click", addWork);
+
+    loadGalleryModal();
 }
 
-const closeModal = function (e){
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = "none"
-    modal.removeEventListener ("click", closeModal)
-    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
-    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-    modal = null
+function closeModal (e){
+    if (modal === null) return;
+    e.preventDefault();
+
+    resetModal();
+
+    modal.style.display = "none";
+    modal.removeEventListener ("click", closeModal);
+    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation);
+    
+    modal = null;
 }
 
-const stopPropagation = function (e){
+function resetModal (){
+    const modalGallery = modal.querySelector(".modal-gallery");
+    const h3modalWrapper = modal.querySelector(".modal-wrapper h3");
+    const addWorkBtn = modal.querySelector(".add-work-btn");
+
+    modalGallery.innerHTML = '';
+    modalGallery.classList.remove("no-border");
+    h3modalWrapper.innerHTML = 'Galerie photo';
+    addWorkBtn.style.display = 'block';
+}
+
+function stopPropagation (e){
     e.stopPropagation()
 }
 
-document.querySelectorAll("a.js-link-modal").forEach (a => {
-    a.addEventListener("click", openModal)
-})
+function createModal (){
+    document.querySelectorAll("a.js-link-modal").forEach (a => {
+        a.addEventListener("click", openModal);
+    })
+}
 
-//Suppresion travaux depuis la modale
+createModal();
 
-fetch("http://localhost:5678/api/works")
-  .then(dataWorks => dataWorks.json())
-  .then (jsonlistWorks => {
+function loadGalleryModal (){
+    fetch("http://localhost:5678/api/works")
+        .then(dataWorks => dataWorks.json())
+        .then (jsonlistWorks => {
 
     const editGallery = document.querySelector(".modal-gallery");
 
-      jsonlistWorks.forEach(work => {
+        jsonlistWorks.forEach(work => {
 
-        const removeWork=document.createElement("div");
-        removeWork.dataset.workId = work.id
-        removeWork.classList.add("remove-work")
+            const removeWork=document.createElement("div");
+            removeWork.dataset.workId = work.id
+            removeWork.classList.add("remove-work")
 
-        const img=document.createElement("img");
-        img.classList.add("img-modal");
-        img.src = work.imageUrl;
+            const img=document.createElement("img");
+            img.classList.add("img-modal");
+            img.src = work.imageUrl;
 
-        const icon = document.createElement("i");
-        icon.classList.add("fa-solid", "fa-trash-can");
+            const icon = document.createElement("i");
+            icon.classList.add("fa-solid", "fa-trash-can");
 
-        removeWork.appendChild(img);
-        removeWork.appendChild(icon);
-        editGallery.appendChild(removeWork);
+            removeWork.appendChild(img);
+            removeWork.appendChild(icon);
+            editGallery.appendChild(removeWork);
 
-
-icon.addEventListener("click", () => {
-
-    const workId = work.id;
-    const confirmation = confirm("Êtes-vous sûr de vouloir supprimer ce travail ?");
+    icon.addEventListener("click", () => {
+        const workId = work.id;
+        const confirmation = confirm("Êtes-vous sûr de vouloir supprimer ce travail ?");
+                    
+            if (confirmation){
+                fetch (`http://localhost:5678/api/works/${work.id}`,{
+                    method : 'DELETE',
+                    headers : {
+                    "Authorization": "Bearer " + authToken,
+                    }
+                })
+                        
+                .then (response => {
+                    if (response.ok) {
+                        removeWork.remove();
+                    }
+                    else {
+                        alert("Erreur lors de la suppression")
+                    }
+                })
+            }
         
-        if (confirmation){
-            fetch (`http://localhost:5678/api/works/${work.id}`,{
-                method : 'DELETE',
-                headers : {
-                "Authorization": "Bearer " + authToken,
-                }
-            })
-            
-        .then (response => {
-            if (response.ok) {
-                removeWork.remove();
-            }
-            else {
-                alert("Erreur lors de la suppression")
-            }
         })
-    }
-
-    });
-
-});
-
+    })
 })
+}
 
-.catch(error => {
-    console.error("Erreur lors du fetch:", error );
-})
+function addWork (){
+    const modalGallery = modal.querySelector(".modal-gallery");
+    modalGallery.innerHTML = '';
+    modalGallery.classList.add("no-border");
 
+    const h3modalWrapper = modal.querySelector(".modal-wrapper h3");
+    h3modalWrapper.innerHTML = 'Ajout photo';
 
-//Ajout travaux depuis la modale
+    const addWorkBtn = modal.querySelector(".add-work-btn");
+    addWorkBtn.style.display = 'none'; 
+}
 
-const addWork = document.querySelector("add-work-btn");
-
-addWork.addEventListener("click", () => {
-
-    
-
-
-})
